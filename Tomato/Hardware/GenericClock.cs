72e0ae717d7b38e3aeeb49b2da8d1c6e653c06d5
Drew DeVault <sir@cmpwn.com>
2012-05-24 20:14:01 -0600

    Fixed clock bug and added numpad +-*/ to keyboard

---
+++ Tomato/Hardware/GenericClock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Tomato.Hardware
+{
+    public class GenericClock : Device
+    {
+        public ushort InterruptMessage { get; set; }
+        public ushort Frequency { get; set; }
+        public Timer Clock { get; set; }
+        public ushort ElapsedTicks { get; set; }
+
+        public override uint DeviceID
+        {
+            get { return 0x12d0b402; }
+        }
+
+        public override uint ManufacturerID
+        {
+            get { return 0x0; }
+        }
+
+        public override ushort Version
+        {
+            get { return 1; }
+        }
+
+        public override string FriendlyName
+        {
+            get { return "Generic Clock (compatible)"; }
+        }
+
+        public override int HandleInterrupt()
+        {
+            switch (AttachedCPU.A)
+            {
+                case 0:
+                    Frequency = AttachedCPU.B;
+                    Clock = new Timer(Tick, null, (int)(1000 / (60d / Frequency)), (int)(1000 / (60d / Frequency)));
+                    break;
+                case 1:
+                    AttachedCPU.C = ElapsedTicks;
+                    break;
+                case 2:
+                    InterruptMessage = AttachedCPU.B;
+                    break;
+            }
+            ElapsedTicks = 0;
+            return 0;
+        }
+
+        public void Tick(object o)
+        {
+            try
+            {
+                if (InterruptMessage != 0)
+                    AttachedCPU.FireInterrupt(InterruptMessage);
+                ElapsedTicks++;
+            }
+            catch { }
+        }
+
+        public override void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
